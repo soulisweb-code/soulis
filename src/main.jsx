ea39c4292ba66fc2503e8d654e3a2d5d8ad0bf01
SuKeyake <sukeoyakesun@gmail.com>
2025-12-13 22:19:02 +0700

    Update Helmet

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async' // 🔥 เพิ่มบรรทัดนี้
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* 🔥 ครอบ App ด้วย HelmetProvider เพื่อให้ SEO ทำงาน */}
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </StrictMode>,
 )
